10ec4015e4e0b373176f674c47dfa135c45d45b7
3N14C <3n14c@vk.com>
2026-03-01 20:45:31 +0800

    refactor: use named exports and bump to 1.1.1

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 // Экспортируем только компоненты, не stories и не CSS
-export * from './stories/typography'
-export * from './stories/button'
+export { Typography } from './stories/typography'
+export { Button } from './stories/button'
 
 // export type { IButtonProps } from 'src/stories/button/ui/button'
 // export type { ITypographyProps } from 'src/stories/typography/ui/typography'
